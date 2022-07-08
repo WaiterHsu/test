d062ad6621dda1cc360495d923dcc0243c45e212
WaiterHsu <94036395+WaiterHsu@users.noreply.github.com>
2022-07-09 01:12:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,12 +9,17 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://shopee.tw/flash_sale')
-driver.set_window_size(1280,1400)
-#driver.refresh()
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver.get('https://shopee.tw/flash_sale')
+# driver.set_window_size(1280,1400)
+# #driver.refresh()
 
-driver.find_element_by_xpath(f'//*[@id="main"]/div/div[2]/div/div/div/div[3]/div/div/div[1]/ul/li[1]').click()
-time.sleep(2)
+# driver.find_element_by_xpath(f'//*[@id="main"]/div/div[2]/div/div/div/div[3]/div/div/div[1]/ul/li[1]').click()
+# time.sleep(2)
 
-driver.close()
+# driver.close()
+
+lib = Selenium()
+
+lib.open_available_browser('https://www.playsport.cc/predictgame.php?allianceid=1')
+lib.close_browser()
